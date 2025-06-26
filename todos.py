9a84e611d068d9c8c51bbe50266e7e26170da6c7
Yash Modi <modiyash2007@gmail.com>
2025-06-26 14:22:04 -0400

    To-Do list

---
+++ todos.py
@@ -0,0 +1,47 @@
+import sys
+# Makes to_do_list file if it doesn't exist
+# Defaults to 1. Walk the Dog, 2. Buy cheese
+try:
+    with open("to_do_list.txt", "x") as file:
+        file.write("1. Walk the Dog\n2. Buy cheese")
+except FileExistsError as e:
+    pass
+except Exception as e:
+    print("An unknown error occurred:", e)
+    sys.exit(1)
+
+if len(sys.argv)==1:
+    try:
+        num_line = 1
+        with open("to_do_list.txt","r") as file:
+            lines = file.readlines()
+            print("Here's your ToDo list:\n")
+            for line in lines:
+                print(f"{num_line}. {line}", end="")
+                num_line+=1
+
+    except Exception as e:
+        print("An unknown error occurred:", e)
+        sys.exit(1)
+elif sys.argv[1]=="add":
+    try:
+        with open("to_do_list.txt", "a") as file:
+            file.write('\n'+sys.argv[2])
+    except Exception as e:
+        print("An unknown error occurred:", e)
+
+# Remove Todo
+
+# Save File
+
+# Print List
+
+# print("\nHere's your ToDo list:\n")
+# print("1. Walk the Dog")
+# print("2. Buy cheese")
+
+# Print Commands
+print("\n\n*********************************\n")
+print(f"To view ToDos:\n{sys.argv[0]}")
+print(f"\nTo add a ToDo:\n{sys.argv[0]} add \"Clean Room\"\n")
+print(f"To remove or complete ToDo:\n{sys.argv[0]} remove 2\n")
